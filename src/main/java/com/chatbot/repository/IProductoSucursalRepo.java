8c772d467b7bef185878550053e5e4ec805ba055
pokeyova <yovanita282.gcg65@gmail.com>
2019-12-05 05:20:55 -0400

    Create Repostory of ProductoSucursalRepo

---
+++ src/main/java/com/chatbot/repository/IProductoSucursalRepo.java
@@ -0,0 +1,8 @@
+package com.chatbot.repository;
+
+import com.chatbot.model.ProductoSucursal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IProductoSucursalRepo extends JpaRepository<ProductoSucursal,Integer> {
+
+}
