56cf06aff6d7acfe12d94d556927b90743ea263a
DANITZA18 <danitzamarlenechurasocujayo@gmail.com>
2019-12-10 19:12:56 -0400

    subiendo la accion mostrar para los empleados

--- src/main/java/com/chatbot/bot/MainBot.java
+++ src/main/java/com/chatbot/bot/MainBot.java
@@ -536,4 +536,30 @@ public class MainBot extends  TelegramLongPollingBot{
                     accion = "ELIMINAR";
                 }
             }
+        }if(comando.equals("4. MOSTRAR") || accion.equals("MOSTRAR"))
+    {
+        if(seccion == "EMPLEADOS"){
+            if(accion.equals("MOSTRAR"))
+            {
+                texto_mensaje = serviceEmpleado.muestraEmpleado(Integer.parseInt(comando));
+                if(texto_mensaje == "")
+                {
+                    texto_mensaje = "NO SE ENCONTRÓ NINGUN EMPLEADO CON ESE ID. POR FAVOR INGRESE OTRO ID";
+                }
+                else{
+                    accion = "";
+                }
+            }
+            else{
+                texto_mensaje = "INGRESE EL ID DEL EMPLEADO QUE QUIERE VER";
+                accion = "MOSTRAR";
+            }
         }
+        else{
+            if(accion.equals("MOSTRAR"))
+            {
+                texto_mensaje = serviceProducto.muestraProducto(comando);
+                if(texto_mensaje == "")
+                {
+                    texto_mensaje = "NO SE ENCONTRÓ NINGUN PRODUCTO CON ESE ID/CÓDIGO. POR FAVOR INGRESE OTRO ID/CÓDIGO";
+                }
