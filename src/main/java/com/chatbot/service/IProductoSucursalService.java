5266f070cb2e85705595a398a1dd150c0b80eaac
DANITZA18 <danitzamarlenechurasocujayo@gmail.com>
2019-12-12 10:42:08 -0400

    adicion de tipos de dato String tambien el metodo actualizar stock

--- src/main/java/com/chatbot/service/IProductoSucursalService.java
+++ src/main/java/com/chatbot/service/IProductoSucursalService.java
@@ -1,5 +1,12 @@
 package com.chatbot.service;
 
+import com.chatbot.model.ProductoSucursal;
+
+import java.util.List;
+
 public interface IProductoSucursalService {
     String listaProductoSucursal();
+   String listaProductosPorSucursal(int sucursal_id);
+
+    void actualizarStock(int sucursal_id,String producto_id, int cantidad);
 }
