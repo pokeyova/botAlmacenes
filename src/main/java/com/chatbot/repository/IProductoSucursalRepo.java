b7358963b9e9075ff366e6ee772fe04135dd21e1
DANITZA18 <danitzamarlenechurasocujayo@gmail.com>
2019-12-12 10:36:50 -0400

    adicion de importaciones en el repository, queries

--- src/main/java/com/chatbot/repository/IProductoSucursalRepo.java
+++ src/main/java/com/chatbot/repository/IProductoSucursalRepo.java
@@ -1,8 +1,19 @@
 package com.chatbot.repository;
 
+package com.chatbot.repository;
+
 import com.chatbot.model.ProductoSucursal;
+import com.chatbot.model.Productos;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface IProductoSucursalRepo extends JpaRepository<ProductoSucursal,Integer> {
+    @Query(value="select id, producto_id, sucursal_id, stock, fecha_reg from producto_sucursal WHERE sucursal_id= :id", nativeQuery = true)
+    List<ProductoSucursal> obtieneProductoSucursal(@Param("id")int id);
 
+    @Query(value="select id, producto_id, sucursal_id, stock, fecha_reg from producto_sucursal WHERE sucursal_id= :sucursal_id AND producto_id = :producto_id", nativeQuery = true)
+    ProductoSucursal ProductoSucursalInfo(@Param("sucursal_id")int sucursal_id, @Param("producto_id")int producto_id);
 }
