d089ae589bf7d980a83a74f8562285c29a7ddca2
DANITZA18 <danitzamarlenechurasocujayo@gmail.com>
2019-12-12 11:29:44 -0400

    adicion de la libreria y tabien de la clase listaproductosSucursal con tipo de dato string (asi se mostrara en el tlegram)

--- src/main/java/com/chatbot/service/ProductoSucursalService.java
+++ src/main/java/com/chatbot/service/ProductoSucursalService.java
@@ -1,11 +1,14 @@
 package com.chatbot.service;
 
 import com.chatbot.model.ProductoSucursal;
+import com.chatbot.model.Productos;
 import com.chatbot.repository.IProductoRepo;
 import com.chatbot.repository.IProductoSucursalRepo;
 import com.chatbot.repository.ISucursalRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductoSucursalService implements IProductoSucursalService{
@@ -29,4 +32,19 @@ public class ProductoSucursalService implements IProductoSucursalService{
         }
         return lista;
     }
-}
+    @Override
+    public String listaProductosPorSucursal(int sucursal_id) {
+        String lista = "";
+        if(repoPS.obtieneProductoSucursal(sucursal_id).size() > 0)
+        {
+            lista = "LISTA DE PRODUCTOS - " + repoSucursal.findById(sucursal_id).get().getNombre();
+            lista +="\n===================================================";
+            lista +="\nID | CÓDIGO | NOMBRE | STOCK";
+            lista +="\n=================================\n";
+            for(ProductoSucursal ps : repoPS.obtieneProductoSucursal(sucursal_id))
+            {
+                lista += ps.getProducto_id()+" | "+repoProducto.findById(ps.getProducto_id()).get().getCodigo()+ " | "+repoProducto.findById(ps.getProducto_id()).get().getNombre()+" | "+ps.getStock()+"\n";
+            }
+        }
+        return lista;
+    }
